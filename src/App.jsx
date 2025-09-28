e8fbaca22bf3ff9ca106f8393ae60b5822a7c4ec
WENLIN-CHANG <magic8405cigam6@gmail.com>
2025-09-28 20:47:38 +0800

    feat:初步完成一個日曆

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import Calendar from './Calendar';
+
 function App() {
   return (
     <div>
       <h1>無限日曆</h1>
-      <p>無限日曆是一個可以無限滾動的日曆</p>
+      <Calendar />
+    </div>
+  )
+}
+
+function Header(props) {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <p>{props.message}</p>
     </div>
   )
 }
